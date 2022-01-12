0d9c63b6cc94d05cad912abfaede9cb5221dc957
Jillium <86424914+Jillium@users.noreply.github.com>
2022-01-12 14:28:06 -0700

    progress save

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // app.use(require('./routes'));
 
-// mongoose.connect(process.env.MONGODB_URI || )
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 mongoose.set('debug', true);
 
