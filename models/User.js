3cc5594a31e21076d86fb001066b992d2eb53c29
Jillium <86424914+Jillium@users.noreply.github.com>
2022-01-13 13:40:10 -0700

    user routes tested and working

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new Schema({
         type: String,
         required: true,
         unique: true,
-        // trim: true
+        trim: true
     },
     email: {
         type: String,
